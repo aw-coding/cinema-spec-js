8fd8edea9ece52c116d75348c5c8f9916b88d963
Andrew Wood <andrewwood90@hotmail.com>
2021-01-07 15:27:24 +0000

    ten tests passed

--- specs/cinema_spec.js
+++ specs/cinema_spec.js
@@ -51,7 +51,11 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, expected)
   });
 
-  it('should be able to check whether there are no films from a particular year');
+  it('should be able to check whether there are no films from a particular year', function () {
+    const actual = cinema.findFilmsByYear(2025);
+    const expected = []
+    assert.deepStrictEqual(actual, expected)
+  });
   it('should be able to check whether all films are over a particular length');
   it('should be able to calculate total running time of all films');
 
